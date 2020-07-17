a7f03a222a8563daf11bd248c98e60c4c09f246d
Milan Felix Šulc <rkfelix@gmail.com>
2020-07-17 12:23:14 +0200

    Revert "Fix CWE ID 327 error"
    This reverts commit 59804ec2302dd01a7b7db01c01b9d4122e8c5375.

--- src/nusoap.php
+++ src/nusoap.php
@@ -8474,7 +8474,7 @@ class nusoap_wsdlcache {
      * @access private
      */
     function obtainMutex($filename, $mode) {
-        if (isset($this->fplock[password_hash($filename, PASSWORD_DEFAULT)])) {
+        if (isset($this->fplock[md5($filename)])) {
             $this->debug("Lock for $filename already exists");
             return false;
         }
